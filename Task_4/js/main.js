3d17bc732310b1eb539887289951531b4b45be88
vealeks <aleksandr.velikiy@w3.ua>
2014-12-26 20:10:05 +0200

    1

--- Task_4/js/main.js
+++ Task_4/js/main.js
@@ -2,7 +2,7 @@
 function unitPage() {
 	sameHeigthBlocks('block1', 'block2');
 	sameHeigthBoxes('list-holder');
-	
+	sameHeigthDivs('holder', 'element', 'container', 'my-box');
 
 }
 
@@ -23,17 +23,31 @@ function sameHeigthBoxes(element){
 
 	for (var i = boxes.length - 1; i >= 0; i--) {
 		var heightBox = getComputedStyle(boxes[i]).height;
-
 		array.push(parseInt(heightBox));
-	}
-
+	};
 	array.sort(function(a,b){return a < b;});
 	maxHeight = array[0] + 'px';
-
 	for (var j = boxes.length - 1; j >= 0; j--){
 		boxes[j].style.height = maxHeight;
-	}
+	};
+}
 
+function sameHeigthDivs(parent , element1, element2, element3) {
+	var parentDivs = document.getElementsByClassName(parent);
+
+	console.log(parentDivs);
+
+	for (var i = 0; i < parentDivs.length; i++) {
+		
+		var divs = parentDivs[i].children;
+		console.log(divs);
+		for (var j = 0; j < divs.length; j--) {
+
+				var height1 = getComputedStyle(divs[j]);
+				console.log(height1);
+
+		};
+	}
 }
 
 if(window.addEventListener) window.addEventListener('load', unitPage, false);
