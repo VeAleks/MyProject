1a2a758c593d8bfea7c0383de811d0ba160a630a
vealeks <aleksandr.velikiy@w3.ua>
2014-12-29 12:34:00 +0200

    ready

--- Task_4/js/main.js
+++ Task_4/js/main.js
@@ -21,10 +21,7 @@ function sameHeigthBoxes(element){
 	maxHeight,
 	array = [];
 
-	for (var i = boxes.length - 1; i >= 0; i--) {
-		var heightBox = getComputedStyle(boxes[i]).height;
-		array.push(parseInt(heightBox));
-	}
+	findHeight (boxes, array);
 	array.sort(function(a,b){return a < b;});
 	maxHeight = array[0] + 'px';
 	for (var j = boxes.length - 1; j >= 0; j--){
@@ -39,38 +36,35 @@ function sameHeigthDivs(element1, element2, element3) {
 		maxHeight,
 		array = [];
 	
-	for (var i = 0; i < classElement1.length; i++) {
-		var heightBox = getComputedStyle(classElement1[i]).height,
-			paddingTop = getComputedStyle(classElement1[i]).paddingTop,
-			paddingBottom = getComputedStyle(classElement1[i]).paddingBottom;
-		array.push(parseInt(heightBox) - parseInt(paddingTop) - parseInt(paddingBottom));
-	}
-	for (var j = 0; j < classElement1.length; j++) {
-		var heightBox = getComputedStyle(classElement2[j]).height,
-			paddingTop = getComputedStyle(classElement2[j]).paddingTop,
-			paddingBottom = getComputedStyle(classElement2[j]).paddingBottom;
-		array.push(parseInt(heightBox)- parseInt(paddingTop) - parseInt(paddingBottom));
-	}
-	for (var k = 0; k < classElement1.length; k++) {
-		var heightBox = getComputedStyle(classElement3[k]).height,
-			paddingTop = getComputedStyle(classElement3[k]).paddingTop,
-			paddingBottom = getComputedStyle(classElement3[k]).paddingBottom;
-		array.push(parseInt(heightBox) - parseInt(paddingTop) - parseInt(paddingBottom));
-	}
+	findHeight(classElement1, array);
+	findHeight(classElement2, array);
+	findHeight(classElement3, array);
 	array.sort(function(a,b){return a < b;});
-	console.log(array);
 	maxHeight = array[0];
-	console.log(classElement1);
-	//classElement1.style.height = maxHeight;
+	addHeight(classElement1, maxHeight);
+	addHeight(classElement2, maxHeight);
+	addHeight(classElement3, maxHeight);
+}
+
+
+function findHeight (element, array) {
+	for (var i = 0; i < element.length; i++) {
+		var heightBox = element[i].offsetHeight;
+
+		array.push(parseInt(heightBox));
+	}
+}
+function addHeight (element, maxHeight){
+	for (var i = 0; i < element.length; i++) {
+		var paddingTop = parseInt(getComputedStyle(element[i]).paddingTop),
+			paddingBottom = parseInt(getComputedStyle(element[i]).paddingBottom),
+			borderTop = parseInt(getComputedStyle(element[i]).borderTop),
+			borderBottom = parseInt(getComputedStyle(element[i]).borderBottom),
+			resultHeight;
+
+		resultHeight = maxHeight - paddingTop - paddingBottom - borderTop - borderBottom;
+		element[i].style.height = resultHeight + 'px';
 	
-	for (var l = 0; l < classElement1.length; l++) {
-			classElement1[l].style.height = maxHeight - getComputedStyle(classElement1[l]).paddingTop - getComputedStyle(classElement1[l]).paddingBottom;
-	}
-	for (var m = 0; m < classElement2.length; m++) {
-			classElement2[m].style.height = maxHeight - getComputedStyle(classElement2[m]).paddingTop - getComputedStyle(classElement2[m]).paddingBottom;
-	}
-	for (var n = 0; n < classElement3.length; n++) {
-			classElement3[n].style.height = maxHeight - getComputedStyle(classElement3[n]).paddingTop - getComputedStyle(classElement3[n]).paddingBottom;
 	}
 }
 
