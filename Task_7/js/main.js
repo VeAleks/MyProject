cd8ba72a7e94193e1c7047535225d885b18405a9
Aleksandr Velikiy <aleksandr.velikiy@w3.ua>
2014-12-31 14:41:05 +0200

    ready

--- Task_7/js/main.js
+++ Task_7/js/main.js
@@ -5,77 +5,55 @@ function unitPage() {
 	for (var i = 0; i< arrayHolders.length; i++) {
 		var holder = arrayHolders[i];
 
-		console.log(holder);
-		if(holder){
+		if(holder.className === 'holder active' ){
+			for (var j = 0; j< holder.children.length; j++) {
+				var child = holder.children[j];
 
+				if(child.className === 'box') child.style.display = 'block'; 
+			}
 		}
 		for (var j = 0; j < holder.children.length; j++) {
 			var child = holder.children[j],
-			btnClick = function (element, active){
-				if(child.addEventListener) child.addEventListener('click', addClass(element, active), false);
-				else if(child.attachEvent) child.attachEvent('onclick', addClass(element, active));
+			btnClick = function (element, active, box){
+				if(child.addEventListener) child.addEventListener('click', addClass(element, active, box), false);
+				else if(child.attachEvent) child.attachEvent('onclick', addClass(element, active, box));
 			};
-			
-			if(child.className === 'button'){
-				btnClick(holder, 'active');
 
-			}
+			if(child.className === 'button') btnClick(holder, 'active', 'box');
 		}
 	}
 }
 
-function addClass(elem, cname) {
-	return function(){
-		var hasClass = function (elem, cname) {
+function addClass(elem, cname, box) {
+	var hasClass = function (elem, cname, box ) {
 			var newArray = elem.className.split(' ');
 
 			for (var i = 0; i < newArray.length; i++){
-				if(newArray[i] === cname){
-					return true;
-				}
+			if(newArray[i] === cname) return true;
 			}
 			newArray.join('');
-		};
+	},
+		boxVisible = function (elem, box){
+		for (var i = 0; i < elem.children.length; i++) {
+			var child = elem.children[i];
 
-		hasClass(elem, cname);
-		if(hasClass(elem, cname) === true) elem.className = elem.className.replace(' ' + cname, '');
-		else {
-			elem.className += ' ' + cname;
-			for (var j = 0; j < elem.children.length; j++){
-				console.log(elem, elem.children[j]);
-				if(elem.children[j] === 'box'){
-					console.log(4);
-					elem.children[j].style.display = 'block';
+			if(hasClass(elem, cname) === true){
+				if(child.className === box) child.style.display = 'block';
+			} else{
+				if(child.className === box){
+					child.style.display = 'none'; 
 				}
 			}
-			
 		}
+	};
+
+	return function(){
+		hasClass(elem, cname, box);
+		if(hasClass(elem, cname) === true) elem.className = elem.className.replace(' ' + cname, '');
+		else elem.className += ' ' + cname;
+		boxVisible(elem, box);
 	}
 }
-
-// function addClass(elem, cname) {
-// 	if (elem.className === '') elem.className += cname;
-// 	else elem.className += ' ' + cname;
-// }
-
-// function hasClass(elem, cname) {
-// 	var newArray = elem.className.split(' ');
-
-// 	for (var i = 0; i < newArray.length; i++){
-// 		if(newArray[i] === cname){
-// 			return true;
-// 		}
-// 	}
-// 	newArray.join('');
-// }
-
-// function removeClass(elem, cname) {
-// 	if(hasClass(elem, cname) === true){
-// 		elem.className = elem.className.replace(cname, '');
-// 	}
-// }
-
-
 
 if(window.addEventListener) window.addEventListener('load', unitPage, false);
 else if(window.attachEvent) window.attachEvent('onload', unitPage);
