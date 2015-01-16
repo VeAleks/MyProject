d5fc0f1353ae7096fa6eb450ea0b9c7cca7c2215
vealeks <aleksandr.velikiy@w3.ua>
2015-01-16 20:10:02 +0200

    up

--- Task_9/js/main2.js
+++ Task_9/js/main2.js
@@ -3,94 +3,91 @@ function unitPage() {
 	var slideset = document.getElementById('gallery').children,
 		button = document.getElementById('btn'),
 		delay = 1000,
-		firstClassActive = slideset[0].className,
 		currentIndex;
 
-	
-		addClassFirstItem(slideset, 'active');
+	addClassFirstItem(slideset, 'active');
+	btnClick(button, move(button, delay, 'btn-active'));
+	console.dir(button);
+	console.log(button.innerText);
+}
 
-	// 	setInterval(
-	// 		function () {
-	// 			if(hasClass(slideset) === true){
-	// 				console.log(slideset);
-	// 				for (var i = 0; i < slideset.length; i++) {
-	// 					console.log(slideset[i].className);
-
-	// 					if (slideset[i].className === 'active') {
-	// 						currentIndex = i;
-	// 						nextIndex = i + 1;
-	// 					//	arr = arr.join(slideset[i].className);
-	// 						slideset[i].className = slideset[i].className.replace('active', '');
-	// 					}
-
-
-	// 				};
-	// 				console.log(currentIndex);
-	// 				//var arr2 = slideset.slice(0, currentIndex );
-					
-	// 			}
-	// 		}, delay);
+function addClassFirstItem (element, cname) {
+	searchClass (element, cname);
+	if(searchClass (element, cname) !== true){
+		addClass(element[0], cname);
+	}
 }
 
 function searchClass (element, cname) {
 	for (var i = 0; i < element.length; i++) {
 		var child = element[i],
-		result = false;
-	//	console.log(child);
+			result = false;
 
 		hasClass(child, cname);
 		if (hasClass(child, cname) === true) {
 			result = true;
-		//	console.log('return', hasClass(child, cname));
 			break;
 		}
 	}
 	return result;
 }
 
-function addClassFirstItem (element, cnum) {
-	searchClass (element, cnum);
-	if(searchClass (element, cnum) !== true){
-		if(element[0].className =='')element[0].className += '' + cnum;
-		else element[0].className += ' ' + cnum;
-	}
-}
-
-function hasClass(elem, cname) {
-	var	newArray = elem.className.split(' '),
+function hasClass(element, cname) {
+	var	newArray = element.className.split(' '),
 		result = false;
 	
 	for (var i = 0; i < newArray.length; i++){
-	//	console.log(i, newArray[i]);
-		if(newArray[i] === cname){
-			result =  true;
-		}
+		if(newArray[i] === cname) result =  true;
 	}
 	newArray.join('');
 	return result;
 }
 
-function addClass(elem, cname) {
-	if (elem.className = '') {
-		elem.className += '' + cname;
-	} else elem.className += ' ' + cname;
+function addClass(element, cname) {
+	if (element.className == '') element.className += '' + cname;
+	else element.className += ' ' + cname;
 }
 
 function removeClass(elem, cname) {
-
 	if(hasClass(elem, cname) === true){
 		elem.className = elem.className.replace(cname, '');
 	}
 }
 
-function changeClass (argument) {
-	
+function move (button, delay, btnClass) {
+	return function () {
+		btnTextChange(button, 'STOP')
+		var timer = setTimeout(
+			function run(){
+				console.log('hello');
+				timer = setTimeout(run, delay);
+				if(btnClick(button, move())){
+					return clearTimeout(timer);
+
+				}
+			}
+			, delay);
+		//addClass(button, btnClass);
+		//		});
+		//var timerOur = clearTimeout(timer);
+
+		//;
+		//console.log('155');
+			// removeClass(button, btnClass);
+			// clearTimeout(timer);
+		}
+	}
+
+	//btnClick(element, clearTimeout(timer));
+
+
+function btnTextChange (element, btnText){
+	element.innerText = btnText;
 }
 
-
-function btnClick(func){
-	if (click.addEventListener) click.addEventListener('click', func, false);
-	else if (click.attachEvent) click.attachEvent('onclick', func);
+function btnClick(element, func){
+	if (element.addEventListener) element.addEventListener('click', func, false);
+	else if (element.attachEvent) element.attachEvent('onclick', func);
 }
 
 if(window.addEventListener) window.addEventListener('load', unitPage, false);
